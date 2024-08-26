33e516121274df4fdb307b9502c45cd44d69949f
Hữu Phúc <20521770@gm.uit.edu.vn>
2024-08-26 21:24:54 +0700

    Create, Read, Delete User

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,153 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+
+import {
+    getAllUsers,
+    createNewUserService,
+    deleteUserService,
+} from '../../services/userService';
+import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
+
+import './UserManage.scss';
+
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false,
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        await this.getAllUsersFromFrontEnd();
     }
 
+    getAllUsersFromFrontEnd = async () => {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users,
+            });
+        }
+    };
+
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        });
+    };
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        });
+    };
+
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUserService(data);
+
+            if (response && response.errCode !== 0) {
+                alert(response.message);
+            } else {
+                await this.getAllUsersFromFrontEnd();
+                this.setState({
+                    isOpenModalUser: false,
+                });
+                emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    handleDeleteUser = async (user) => {
+        try {
+            let response = await deleteUserService(user.id);
+
+            if (response && response.errCode === 0) {
+                await this.getAllUsersFromFrontEnd();
+            } else {
+                alert(response.errMessage);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     render() {
+        let arrUsers = this.state.arrUsers;
+
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    createNewUser={this.createNewUser}
+                />
+                <div className="title text-center">MANAGE USERS</div>
+                <div className="mx-2">
+                    <button
+                        className="btn btn-primary px-3"
+                        onClick={() => this.handleAddNewUser()}
+                    >
+                        <i className="fas fa-plus mx-2"></i>
+                        Add new users
+                    </button>
+                </div>
+                <div className="users-table mt-4 mx-2">
+                    <table id="customers">
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
+                            {arrUsers &&
+                                arrUsers.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className="btn btn-edit">
+                                                    <i className="fas fa-pencil-alt"></i>
+                                                </button>
+                                                <button
+                                                    className="btn btn-delete"
+                                                    onClick={() =>
+                                                        this.handleDeleteUser(
+                                                            item
+                                                        )
+                                                    }
+                                                >
+                                                    <i className="fas fa-trash-alt"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,25 @@ const handleOnClickLogin = (email, password) => {
     return axios.post('/api/login', { email, password });
 };
 
-export { handleOnClickLogin };
+const getAllUsers = (id) => {
+    return axios.get(`/api/get-all-users?id=${id}`);
+};
+
+const createNewUserService = (data) => {
+    return axios.post('/api/create-new-user', data);
+};
+
+const deleteUserService = (id) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: id,
+        },
+    });
+};
+
+export {
+    handleOnClickLogin,
+    getAllUsers,
+    createNewUserService,
+    deleteUserService,
+};
